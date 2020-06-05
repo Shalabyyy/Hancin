be782393c78d2d3a9f29c5e4cba7e8a33ea647ce
Shalabyyy <youshalaby@gmail.com>
2020-06-05 22:20:20 +0200

    Added CORS

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 4000;
 const path = require("path");
 const cors = require("cors");
 app.use(express.json());
